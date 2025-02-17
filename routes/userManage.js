ee4219739ee3fc49d6d0756d20c75a431d80e7ce
Chucky.Tsai <64692566+chuckytsai@users.noreply.github.com>
2025-02-17 10:08:34 +0800

    [ feature ] 使用者管理清單API,改為進入連結,獲取資料後關閉

--- routes/userManage.js
+++ routes/userManage.js
@@ -3,8 +3,6 @@ var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 var router = express.Router();
 
-let userManageList = [];
-
 var config = {
   server: process.env['DB_HOST'],
   authentication: {
@@ -21,53 +19,47 @@ var config = {
   }
 };
 
-var connection = new Connection(config);
-connection.on('connect', function (err) {
-  // If no error, then good to proceed.
-  if (err) {
-    console.log("Connection Failed");
-    throw err;
-  }
-  executeStatement();
-});
-
-connection.connect();
-
-function executeStatement() {
-  request = new Request("SELECT TOP (1000) [Id],[Name],[NameEng],[EmployeeId],[Title],[IsEnabled],[IsAdmin] FROM [TmcRobo-Latest].[dbo].[User]", function (err) {
-    if (err) {
-      console.log(err)
-    }
-  });
-  userManageList = [];
-
-  request.on("row", (columns) => {
-    const datas = {};
-    datas["userId"] = columns[0].value;
-    datas["userName"] = columns[1].value;
-    datas["userNmaeEng"] = columns[2].value;
-    datas["employeeId"] = columns[3].value;
-    datas["title"] = columns[4].value;
-    datas["isEnabled"] = columns[5].value;
-    datas["isAdmin"] = columns[6].value;
-    userManageList.push(datas);
-  });
-  connection.execSql(request);
-
-}
-connection.cancel();
-
 /* GET home page. */
 router.get('/list', function (req, res, next) {
-  res.render('api', {
-    obj: JSON.stringify({
-      code: 200,
-      message: null,
-      data: {
-        list: userManageList
+  var userManageList = [];
+  var connection = new Connection(config);
+  connection.on('connect', function await(err) {
+    // If no error, then good to proceed.
+    if (err) {
+      console.log("Connection Failed");
+      throw err;
+    }
+    request = new Request("SELECT TOP (1000) [Id],[Name],[NameEng],[EmployeeId],[Title],[IsEnabled],[IsAdmin] FROM [TmcRobo-Latest].[dbo].[User]", function (err) {
+      if (err) {
+        console.log(err)
       }
-    })
+
+      res.render('api', {
+        obj: JSON.stringify({
+          code: 200,
+          message: null,
+          data: [...userManageList]
+        })
+      });
+    });
+
+    request.on("row", (columns) => {
+      const datas = {};
+      datas["userId"] = columns[0].value;
+      datas["userName"] = columns[1].value;
+      datas["userNmaeEng"] = columns[2].value;
+      datas["employeeId"] = columns[3].value;
+      datas["title"] = columns[4].value;
+      datas["isEnabled"] = columns[5].value;
+      datas["isAdmin"] = columns[6].value;
+      userManageList.push(datas);
+    });
+    connection.execSql(request);
   });
+
+  connection.connect();
+
+  connection.cancel();
 });
 
 module.exports = router;
