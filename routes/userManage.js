1860abe11dc56007f780256d5c73d3272c69750c
Chucky.Tsai <64692566+chuckytsai@users.noreply.github.com>
2025-02-18 13:39:37 +0800

    [ feature ] 使用者管理新增改為post

--- routes/userManage.js
+++ routes/userManage.js
@@ -16,10 +16,11 @@ var config = {
     encrypt: false,
     trustServerCertificate: true,
     enableArithAbort: true,
+    database: process.env['DB_BASE']
   }
 };
 
-/* GET home page. */
+/* 使用者管理清單. */
 router.get('/list', function (req, res, next) {
   var userManageList = [];
   var connection = new Connection(config);
@@ -32,7 +33,7 @@ router.get('/list', function (req, res, next) {
       });
       throw err;
     }
-    request = new Request("SELECT TOP (1000) [Id],[Name],[NameEng],[EmployeeId],[Title],[IsEnabled],[IsAdmin] FROM [TmcRobo-Latest].[dbo].[User]", function (err) {
+    request = new Request("SELECT * FROM [User]", function (err) {
       if (err) {
         res.json({
           code: 500,
@@ -41,10 +42,73 @@ router.get('/list', function (req, res, next) {
       }
 
       res.json({
-          code: 200,
-          message: null,
-          data: [...userManageList]
+        code: 200,
+        message: null,
+        data: [...userManageList]
+      });
+    });
+
+    request.on("row", (columns) => {
+      const datas = {};
+      datas["userId"] = columns[0].value;
+      datas["userName"] = columns[1].value;
+      datas["userNmaeEng"] = columns[2].value;
+      datas["employeeId"] = columns[3].value;
+      datas["title"] = columns[4].value;
+      datas["isEnabled"] = columns[6].value;
+      datas["isAdmin"] = columns[7].value;
+      userManageList.push(datas);
+    });
+    connection.execSql(request);
+  });
+
+  connection.connect();
+
+  connection.cancel();
+});
+
+// 新增使用者
+router.post('/add', function (req, res, next) {
+
+  
+  console.log(req["_startTime"])
+  console.log(req.body)
+  res.json({
+    code: 200,
+    message: null,
+  });
+});
+
+// 編輯使用者
+router.get('/edit', function (req, res, next) {
+  var userManageList = [];
+  var connection = new Connection(config);
+  connection.on('connect', function await(err) {
+    // If no error, then good to proceed.
+    if (err) {
+      res.json({
+        code: 500,
+        message: err,
+      });
+      throw err;
+    }
+
+    // INSERT [User] (Id, Name, NameEng, EmployeeId, Title, Password, IsEnabled, IsAdmin, UpdatedTime, AreaId, EMailAddress, Setting)
+    // VALUES ('00000000-0000-0000-0000-000000000001', 'Test', '','000002', '管理者', 'BriJvAP8DqiRFdaMLsQCFQ==', 1, 1, '2015-03-11 00:00:00.000', NULL, NULL, NULL)
+
+    request = new Request("SELECT * FROM [TmcRobo-Latest].[dbo].[User]", function (err) {
+      if (err) {
+        res.json({
+          code: 500,
+          message: err,
         });
+      }
+
+      res.json({
+        code: 200,
+        message: null,
+        data: [...userManageList]
+      });
     });
 
     request.on("row", (columns) => {
