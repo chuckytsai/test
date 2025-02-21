b47973246c250ce0a86f2c2d74d46aa3326341c2
Chucky.Tsai <64692566+chuckytsai@users.noreply.github.com>
2025-02-21 17:19:49 +0800

    [ feature ] 檢驗歷程單筆查詢

--- routes/historyManage.js
+++ routes/historyManage.js
@@ -43,7 +43,7 @@ router.post('/list', function (req, res, next) {
       throw err;
     }
 
-    const sql = "SELECT DISTINCT ej.Id [Id],[PatientName],[JobTypeId],[ExamineDate],[QueueNo],[WaitingSecond],[ServiceSecond],ej.Status,us.Name FROM [TmcRobo-Latest].[dbo].[ExamineJob] as ej" + "\n";
+    const sql = "SELECT DISTINCT ej.Id [Id],[PatientName],[JobTypeId],[ExamineDate],[QueueNo],[WaitingSecond],[ServiceSecond],ej.Status,us.Name FROM [TmcRobo-Latest].[dbo].[ExamineJob] as ej LEFT JOIN [TmcRobo-Latest].[dbo].[ExamineRecord] As er" + "\n";
     const jobId = "ON ej.Id = er.JobId" + "\n";
     const joninUser = "LEFT JOIN [TmcRobo-Latest].[dbo].[User] as us" + "\n";
     const userName = "ON us.Id = er.UserId" + "\n";
@@ -105,8 +105,7 @@ router.post('/list', function (req, res, next) {
 
 /* 單筆檢驗項目清單. */
 router.post('/detailList', function (req, res, next) {
-  const reqExamineJobId = req.body.examineJobId ? "'" +  req.body.examineJobId + "'\n" : "'00000000-0000-0000-0000-000000000000'" + "\n"
-
+  const reqExamineJobId = req.body.examineJobId ? "'" +  req.body.examineJobId + "'\n" : "'00000000-0000-0000-0000-000000000000'" + "\n";
   const list = [];
   const connection = new Connection(config);
   connection.on('connect', function await(err) {
@@ -119,10 +118,10 @@ router.post('/detailList', function (req, res, next) {
       throw err;
     }
     const sql = "SELECT ejd.Id,ejd.ExamineJobId,ejd.Status,ejd.HisRawData,ejd.Barcode,ejd.UpdatedTime,ej.Status FROM [TmcRobo-Latest].[dbo].[ExamineJobDetail] as ejd LEFT JOIN [TmcRobo-Latest].[dbo].[ExamineJob] As ej" + "\n";
-    const id = "ON ejd.ExamineJobId = ej.Id"+ "\n";
+    const id = "ON ejd.ExamineJobId = ej.Id" + "\n";
     const examineJobId = "WHERE ExamineJobId = " + reqExamineJobId;
     const sotBy = "ORDER BY Barcode ASC";
-    console.log(sql + examineJobId + sotBy)
+    console.log(sql + id + examineJobId + sotBy)
     request = new Request(sql + id +  examineJobId + sotBy, function (err, rows) {
       if (err) {
         res.json({
@@ -172,5 +171,58 @@ router.post('/detailList', function (req, res, next) {
   connection.cancel();
 });
 
+/* 單筆檢驗歷程清單. */
+router.post('/recordList', function (req, res, next) {
+  const reqExamineJobId = req.body.examineJobId ? "'" +  req.body.examineJobId + "'\n" : "'00000000-0000-0000-0000-000000000000'" + "\n"
+
+  const list = [];
+  const connection = new Connection(config);
+  connection.on('connect', function await(err) {
+    // If no error, then good to proceed.
+    if (err) {
+      res.json({
+        code: 500,
+        message: err,
+      });
+      throw err;
+    }
+    const sql = "SELECT * FROM [TmcRobo-Latest].[dbo].[ExamineRecord] as er LEFT JOIN [TmcRobo-Latest].[dbo].[User] As us" + "\n";
+    const id = "ON er.UserId = us.Id"+ "\n";
+    const clerk = "LEFT JOIN [TmcRobo-Latest].[dbo].[Clerk] As ck"+ "\n";
+    const clerkId = "ON er.ClerkId LIKE ck.Id"+ "\n";
+    const examineJobId = "WHERE JobId = " + reqExamineJobId;
+    const sotBy = "ORDER BY CreatedTime ASC";
+    console.log(sql + id +  clerk + clerkId + examineJobId + sotBy)
+    request = new Request(sql + id + clerk + clerkId + examineJobId + sotBy, function (err, rows) {
+      if (err) {
+        res.json({
+          code: 500,
+          message: err,
+        });
+      }
+
+      res.json({
+        code: 200,
+        message: null,
+        data: [...list]
+      });
+    });
+
+    request.on("row", (columns) => {
+      const datas = {};
+      const CreatedTime = Date.parse(columns[7].value);
+      datas["id"] = columns[0].value;
+      datas["status"] = columns[5].value;
+      datas["createdAt"] = Date.parse(dayjs(CreatedTime).set("hour", dayjs(CreatedTime).format("hh") - 8).format("YYYY-MM-DDThh:mm:ss")) / 1000;
+      datas["userName"] = columns[10].value;
+      datas["counterNo"] = columns[24].value;
+      list.push(datas);
+    });
+    connection.execSql(request);
+  });
+  connection.connect();
+
+  connection.cancel();
+});
 
 module.exports = router;
